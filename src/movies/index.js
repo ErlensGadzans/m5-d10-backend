ab37e3bc8fc663038f0b342660a924f43094e66f
ErlensGadzans <erlens.gadzans@users.noreply.github.com>
2020-12-18 15:01:28 +0200

    post method added

--- src/movies/index.js
+++ src/movies/index.js
@@ -1,13 +1,32 @@
 const express = require("express");
 const { readMovies, writeMovies } = require("../library/utilities");
+const uniqid = require("uniqid");
+const { fstat } = require("fs-extra");
+const fs = require("fs");
+const { join } = require("path");
 
 const moviesRouter = express.Router();
+
+const moviesPath = join(__dirname, "movies.json");
 
 moviesRouter.get("/", async (req, res, next) => {
   try {
     const moviesDB = await readMovies();
-    console.log(moviesDB);
+    // console.log(moviesDB);
     res.send(moviesDB);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
+
+moviesRouter.post("/", async (req, res, next) => {
+  try {
+    const moviesDB = await readMovies();
+    const newMovie = { ...req.body, imdbID: uniqid() };
+    moviesDB.push(newMovie);
+    await writeMovies(moviesDB);
+    res.status(201).send(newMovie);
   } catch (err) {
     console.log(err);
     next(err);
