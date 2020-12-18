e7ca6195eef574e1b315e5c7a0d845c78ceaedd3
ErlensGadzans <erlens.gadzans@users.noreply.github.com>
2020-12-18 14:03:52 +0200

    get method added

--- src/movies/index.js
+++ src/movies/index.js
@@ -3,4 +3,15 @@ const { readMovies, writeMovies } = require("../library/utilities");
 
 const moviesRouter = express.Router();
 
-const moviesRouter = express.Router();
+moviesRouter.get("/", async (req, res, next) => {
+  try {
+    const moviesDB = await readMovies();
+    console.log(moviesDB);
+    res.send(moviesDB);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
+
+module.exports = moviesRouter;
